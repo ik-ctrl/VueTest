afaa09ee25767614221f6c2f920d3ef98e32b2ea
ikondratyev <kondratyev.ie@gmail.com>
2021-12-08 21:59:14 +0300

    1. удалил лишние классы 2. добавил библиотеки для версионность 3. начат работа над TaskController

---
+++ SimpleBackend.WebApi/SimpleBackend.WebApi/Options/ConfigureSwaggerOptions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace SimpleBackend.WebApi.Options
+{
+    
+    internal class ConfigureSwaggerOptions:IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
+        {
+            _provider = provider;
+        }
+        
+        public void Configure(SwaggerGenOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- SimpleBackend.WebApi/SimpleBackend.WebApi/Program.cs
+++ SimpleBackend.WebApi/SimpleBackend.WebApi/Program.cs
@@ -9,15 +9,41 @@ using Microsoft.Extensions.Logging;
 
 namespace SimpleBackend.WebApi
 {
+    /// <summary>
+    /// Главный класс приложения
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Запуск приложения
+        /// </summary>
+        /// <param name="args">Параметры командной строки</param>
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
 
+        /// <summary>
+        /// Создания строителя приложения
+        /// </summary>
+        /// <param name="args">Параметры командной строки</param>
+        /// <returns>Строитель приложения</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseKestrel(option =>
+                    {
+                        option.ListenAnyIP(5000);
+                    });
+                });
     }
 }
