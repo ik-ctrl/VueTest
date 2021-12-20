ee6c638858fce5660043b279c29e2382dae979df
KondratievIE <kondratyev.ie@gmail.com>
2021-12-20 13:31:30 +0300

    добавил проект  фабрику для дизайна контекст БД

--- SimpleBackend.WebApi/SimpleBackend.Database/Designed/PostgresDbContextDesignFactory.cs
+++ SimpleBackend.WebApi/SimpleBackend.Database/Designed/PostgresDbContextDesignFactory.cs
@@ -1,10 +1,31 @@
-﻿// namespace SimpleBackend.Database.Designed
-// {
-//     /// <summary>
-//     /// Фабрика для создания контекста на момент миграции
-//     /// </summary>
-//     public class PostgresDbContextDesignFactory:DbContextD
-//     {
-//         
-//     }
-// }
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace SimpleBackend.Database.Designed
+{
+    /// <summary>
+    /// Фабрика для создания контекста на момент миграции
+    /// </summary>
+    public class PostgresDbContextDesignFactory : IDesignTimeDbContextFactory<PostgresDbContext>
+    {
+        /// <summary>
+        /// Создания DbContext для применения миграций
+        /// </summary>
+        /// <param name="args">аругменты командной строки</param>
+        /// <returns>Контекст БД</returns>
+        public PostgresDbContext CreateDbContext(string[] args)
+        {
+            var connection = new PostgresConnection()
+            {
+                AppName = "SimpleBacked.WebApi",
+                ConnectionIdleLifetime = 5,
+                DatabaseName = "SimpleDB",
+                Host = "127.0.0.1",
+                Password = "postgres",
+                Port = 5434,
+                Username = "postgres"
+            };
+            return new PostgresDbContext(connection);
+
+        }
+    }
+}
