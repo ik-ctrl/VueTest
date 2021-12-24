c2471688a1e70b090e4efab192d4454e19a35e2d
KondratievIE <kondratyev.ie@gmail.com>
2021-12-24 13:03:18 +0300

    Добавлена предварительная архитектура api

---
+++ SimpleBackend.WebApi/SimpleBackend.WebApi/Controllers/JobResultController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleBackend.WebApi.Controllers
+{
+    /// <summary>
+    /// Контроллер для возврата результата запроса
+    /// </summary>
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}/[action]")]
+    public class JobResultController:ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult>GetResult() =>  await Task.Run(() => Ok());
+    }
+}
---
+++ SimpleBackend.WebApi/SimpleBackend.WebApi/Controllers/JobStatusController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleBackend.WebApi.Controllers
+{
+    /// <summary>
+    /// Контроллер для проверки статуса задания
+    /// </summary>
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}/[action]")]
+    public class JobStatusController:ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Check() =>  await Task.Run(() => Ok());
+    }
+}
