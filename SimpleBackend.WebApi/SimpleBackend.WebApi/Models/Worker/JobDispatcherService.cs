1841ff4b296ecbdfc8233f45dcfaa56cf738be0e
ikondratyev <kondratyev.ie@gmail.com>
2021-12-30 14:48:47 +0300

    начат работа над dispatcherom

---
+++ SimpleBackend.WebApi/SimpleBackend.WebApi/Models/Worker/JobDispatcherService.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleBackend.WebApi.Models.Worker
+{
+    /// <summary>
+    /// Диспетчер задач
+    /// </summary>
+    public sealed class JobDispatcherService
+    {
+        public bool AddJob() => true;
+
+        public bool CheckStatusJob() => true;
+
+        public bool GetResultJob() => true;
+
+    }
+}
